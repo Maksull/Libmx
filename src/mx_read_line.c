4e9a9e77050c16bd773aa60e00c165589abfadcb
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-10-05 17:45:33 +0300

    Fixes

--- src/mx_read_line.c
+++ src/mx_read_line.c
@@ -2,49 +2,56 @@
 
 int mx_read_line(char **lineptr, size_t buf_size, char delim, const int fd)
 {
-    if (buf_size < 0 || buf_size > 2147483648){
-        return -2;
-    }
-
-    char *buf = mx_strnew(buf_size);
-    if (buf == NULL) {
-        return -2;
-    }
-    char *str = mx_strnew(buf_size);
-    if (str == NULL) {
-        free(buf);
-        return -2;
-    }
-    size_t type;
-
-    if (fd > 0)
+    if (lineptr == NULL || buf_size == 0 || fd < 0) 
     {
-        while ((type = read(fd, buf, buf_size)) > 0)
-        {
-            mx_strcat(str, buf);
-        }
-        if (type < 0)
-        {
-            return -1;
-        }
-        int index = mx_get_char_index(str, delim);
-        if (index == -1)
-        {
-            *lineptr = str;
-
-            return -1;
-        }
-        else
-        {
-            mx_strncpy(*lineptr, str, index);
-        }
-        
-        close(fd);
-        free(buf);
-        free(str);
-
-        return index;
+        return -2;
     }
 
-    return -2;
+    char *buffer = (char *)mx_strnew(buf_size);
+    if (buffer == NULL) 
+    {
+        return -2;
+    }
+
+    size_t bytes_read = 0;
+    while (true) 
+    {
+        char c;
+        ssize_t result = read(fd, &c, 1);
+        if (result == -1)
+        {
+            mx_strdel(&buffer);
+            return -2;
+        } 
+        else if (result == 0 && bytes_read == 0) 
+        {
+            mx_strdel(&buffer);
+            return -1;
+        } 
+        else if (result == 0 || c == delim)
+        {
+            break;
+        } 
+        else 
+        {
+            buffer[bytes_read++] = c;
+        }
+        if (bytes_read >= buf_size - 1) 
+        {
+            buf_size *= 2;
+            char *new_buffer = (char *)mx_strnew(buf_size);
+            if (new_buffer == NULL) 
+            {
+                mx_strdel(&buffer);
+                return -2;
+            }
+            mx_strncpy(new_buffer, buffer, bytes_read);
+            mx_strdel(&buffer);
+            buffer = new_buffer;
+        }
+    }
+    buffer[bytes_read] = '\0';
+    *lineptr = buffer;
+    
+    return bytes_read;
 }
